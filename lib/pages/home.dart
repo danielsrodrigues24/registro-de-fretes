42f38877bde6a2864e0db4753e468a36ee505f8c
Daniel da Silva Rodrigues <danielsrodrigues24@gmail.com>
2025-09-19 17:58:09 -0300

    Enhance Home page UI and add FAB
    Added a floating action button for adding new items, improved AppBar styling, and replaced the body with a search bar and icon inside a container with box shadow. These changes improve usability and visual appeal of the Home page.

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -2,11 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:registro_de_fretes/widgets/shipment_card.dart';
 
 class Home extends StatelessWidget{
+  const Home({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       appBar: AppBar(
-        title: const Text('On the way')),
+        title: const Text('On the way', style: TextStyle(color: Colors.white),),
+        backgroundColor: Colors.blueGrey,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){},
+        child: Icon(Icons.add),
+      ),
       drawer: Drawer(
         child: ListView(
           padding: EdgeInsets.zero,
@@ -26,11 +35,34 @@ class Home extends StatelessWidget{
           ],
         ),
       ),
-      body: Center(
-        child: ShipmentCard(),
+      body: Container(
+        decoration: BoxDecoration(
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withValues(alpha: 0.2), // Cor da sombra
+              spreadRadius: 2,  // Expansão da sombra
+              blurRadius: 6,    // Suavização
+              offset: Offset(3, 3), // Posição da sombra (x, y)
+            )
+          ]
+        ),
+        child: Row(
+          children: [
+            Icon(Icons.edit_calendar),
+            TextField(
+              decoration: InputDecoration(
+                hintText: "Pesquisar...",
+                prefixIcon: Icon(Icons.search),
+                border: OutlineInputBorder(
+                 borderRadius: BorderRadius.circular(12.0)
+                )
+              ),
+              onChanged: (value) {},
+            )
+          ],
+        ),
       ),
     );
   }
-  const Home({super.key});
 }
 
